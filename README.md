4bac92223c622851bed5f7f3816df7a4c4b80217
Manassarn Manoonchai <manassarn@gmail.com>
2021-04-14 15:39:08 +0700

    feat: reorganize projects

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 Active projects :
 
+- [Carpalx-th](https://github.com/narze/carpalx-th) 
+- [Awesome Salim Quotes](https://narze.github.io/awesome-salim-quotes) : 197 Quotes
 - [Digital Garden](https://monosor.com) : 40 Posts
+
+Past projects :
+
 - [Today I Learned](https://github.com/narze/til) : 21 Entries
 - [#100DaysOfCode](https://github.com/narze/100daysofcode) : 100 Days! 🎉
 
-Hacktoberfest Projects :
-
-- [Hacktoberfest EZ](https://github.com/narze/hacktoberfest_ez)
-- [ต่อเพลง](https://github.com/narze/torpleng) (61 เพลง)
-- [Harassless](https://github.com/narze/harassless)
-- [Thailand Hall of #save](https://github.com/narze/thailand-hall-of-save)
-- [toSkoy](https://github.com/narze/toSkoy) & [skoy.js](https://github.com/narze/skoy.js)
-- [PokeMob](https://github.com/narze/PokeMob)
-- [ได้ไหม!?](https://github.com/narze/DaiMai)
-- [awesome-salim-quotes](https://github.com/narze/awesome-salim-quotes) (197 Quotes)
--- writeme.rb
+++ writeme.rb
@@ -13,12 +13,21 @@ til_entries = til_response.body.split("\n")[2].delete("^0-9")
 digital_garden_posts = HTTParty.get("https://monosor.com/api/info.json")
 digital_garden_posts_count = JSON.parse(digital_garden_posts.body)["count"]
 
+asq_res = HTTParty.get("https://raw.githubusercontent.com/narze/awesome-salim-quotes/main/README.md")
+asq_entries = asq_res.body.strip.split("\n").select { |l| l.start_with?("- ") }.size
+
 @active_projects = [
+
+  { name: "Carpalx-th", link: "https://github.com/narze/carpalx-th" },
+  { name: "Awesome Salim Quotes", link: "https://narze.github.io/awesome-salim-quotes", duration: "#{asq_entries} Quotes" },
   {
     name: "Digital Garden",
     link: "https://monosor.com",
     duration: "#{digital_garden_posts_count} Posts",
   },
+]
+
+@past_projects = [
   {
     name: "Today I Learned",
     link: "https://github.com/narze/til",
@@ -34,9 +43,6 @@ digital_garden_posts_count = JSON.parse(digital_garden_posts.body)["count"]
 torpleng_res = HTTParty.get("https://raw.githubusercontent.com/narze/torpleng/main/README.md")
 torpleng_entries = torpleng_res.body.split("# Entries").last.strip.split("\n").select { |l| l.start_with?("- ") }.size
 
-asq_res = HTTParty.get("https://raw.githubusercontent.com/narze/awesome-salim-quotes/main/README.md")
-asq_entries = asq_res.body.strip.split("\n").select { |l| l.start_with?("- ") }.size
-
 @special_projects = {
   torpleng: "(#{torpleng_entries} เพลง)",
   awesome_salim_quotes: "(#{asq_entries} Quotes)",
@@ -47,3 +53,5 @@ template = File.read("writeme.md.erb")
 content = ERB.new(template, nil, "<>").result
 
 File.write("README.md", content)
+
+puts "README updated."
